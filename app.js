1a51fe49dc2c42716a4b109067160795758d5074
Abu Sayed <abus04651@gmail.com>
2024-10-30 18:19:14 +0600

    #update9.124

--- app.js
+++ app.js
@@ -18,19 +18,13 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-const allowedOrigins = ['https://abdullah-shayed.onrender.com','http://localhost:3000'];
+// const allowedOrigins = ['https://abdullah-shayed.onrender.com','http://localhost:3000'];
   
   // Configure CORS with specific origins
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: 'https://abdullah-shayed.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // This is important, allows cookies to be sent with requests
 }));
 
